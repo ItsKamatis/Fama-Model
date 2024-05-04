6ff8d4fc3adaa00cd100a15537719e6c04591d3e
Kamatis <colmenar68@gmail.com>
2024-05-03 21:30:07 -0400

    Pushes

--- Proj/RFM.py
+++ Proj/RFM.py
@@ -1,16 +1,24 @@
-import pandas_datareader
+import pandas_datareader as pdr
 import pandas as pd
+import pandas_datareader.data as web
 import matplotlib.pyplot as plt
 import yfinance as yf
+import datetime as dt
 yf.pdr_override()
-pandas_datareader.famafrench.get_available_datasets()
-start = '2021-12-01'
-ff = pandas_datareader.famafrench.FamaFrenchReader('F-F_Research_Data_Factors', freq='W', start=start).read()
-print(ff)
+pdr.famafrench.get_available_datasets()
+start = dt.datetime(2020, 1, 1)
+end = dt.datetime.today()
+ff = pdr.famafrench.FamaFrenchReader('F-F_Research_Data_Factors', freq='M', start=start).read()
+##print(ff)
 ff_df = ff[0]
 ff_df.plot(subplots=True, figsize=(12,4))
 
-##ff2 = pandas_datareader.famafrench.FamaFrenchReader('F-F_Momentum_Factors_daily', freq='W', start=start).read()
-##ff_mom_df = pandas_datareader.famafrench.FamaFrenchReader('F_F_Momentum_Factor', freq='W', start=start).read()[0]
-##ff_mom_df.plot(subplots=True, figsize=(12,4))
+ff_momentum = pdr.famafrench.FamaFrenchReader('F-F_Momentum_Factor', freq='M', start=start).read()
+ff_mom_df = ff_momentum[0]
+##print(ff_momentum)
 
+ff_merged_df = pd.merge(ff_df, ff_mom_df, on='Date', how='inner', sort=True, copy=True, indicator=False, validate='one_to_one')
+
+### Not working IDK why ###
+NVDA_data = web.DataReader('NVDA', 'yahoo', 'start', 'end')['Adj Close']
+NVDA_data.head()

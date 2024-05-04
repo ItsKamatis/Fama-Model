d1bd7f692f20c892572e58876e798d1c79e5278f
jovalles15 <49589832+jovalles15@users.noreply.github.com>
2024-05-03 20:26:12 -0400

    Update RFM.py

--- Proj/RFM.py
+++ Proj/RFM.py
@@ -4,13 +4,15 @@ import matplotlib.pyplot as plt
 
 pandas_datareader.famafrench.get_available_datasets()
 start = '2021-12-01'
-ff = pandas_datareader.famafrench.FamaFrenchReader('F-F_Research_Data_Factors', freq='W', start=start).read()
-print(ff)
+ff = pandas_datareader.famafrench.FamaFrenchReader(
+    'F-F_Momentum_Factor', freq='W', start=start).read()
+print('F-F_Momentum_Factor')
 ff_df = ff[0]
-ff_df.plot(subplots=True, figsize=(12,4))
+ff_df.plot(subplots=True, figsize=(12, 4))
 
-##ff2 = pandas_datareader.famafrench.FamaFrenchReader('F-F_Momentum_Factors_daily', freq='W', start=start).read()
-##ff_mom_df = pandas_datareader.famafrench.FamaFrenchReader('F_F_Momentum_Factor', freq='W', start=start).read()[0]
-##ff_mom_df.plot(subplots=True, figsize=(12,4))
+# ff2 = pandas_datareader.famafrench.FamaFrenchReader('F-F_Momentum_Factors_daily', freq='W', start=start).read()
+# ff_mom_df = pandas_datareader.famafrench.FamaFrenchReader('F_F_Momentum_Factor', freq='W', start=start).read()[0]
+# ff_mom_df.plot(subplots=True, figsize=(12,4))
 
-NVDA_df = pandas_datareader.get_data_yahoo('NVDA', start=start)['Adj Close'].resample('M').ffill().pct_change()
+# NVDA_df = pandas_datareader.get_data_yahoo('NVDA', start=start)[
+# 'Adj Close'].resample('M').ffill().pct_change()

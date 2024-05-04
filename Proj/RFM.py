bf27369ed67caa7b9ecc815c944bc0faa5346468
Kamatis <colmenar68@gmail.com>
2024-05-03 20:28:38 -0400

    Pushes

--- Proj/RFM.py
+++ Proj/RFM.py
@@ -1,16 +1,14 @@
 import pandas_datareader
 import pandas as pd
 import matplotlib.pyplot as plt
-!pip install yfinance
+import yfinance as yf
+yf.pdr_override()
 pandas_datareader.famafrench.get_available_datasets()
 start = '2021-12-01'
-ff = pandas_datareader.famafrench.FamaFrenchReader('F-F_Research_Data_Factors', freq='W', start=start).read()
-print(ff)
+ff = pandas_datareader.famafrench.FamaFrenchReader('F-F_Research_Data_Factors', freq='M', start=start).read()
+##print(ff)
 ff_df = ff[0]
 ff_df.plot(subplots=True, figsize=(12,4))
 
-##ff2 = pandas_datareader.famafrench.FamaFrenchReader('F-F_Momentum_Factors_daily', freq='W', start=start).read()
-##ff_mom_df = pandas_datareader.famafrench.FamaFrenchReader('F_F_Momentum_Factor', freq='W', start=start).read()[0]
-##ff_mom_df.plot(subplots=True, figsize=(12,4))
+ ff_momentum = pandas_datareader.get_data_famafrench('F-F_Momentum_Factor', freq='M', start=start)
 
-NVDA_df = pandas_datareader.get_data_yahoo('NVDA', start=start)['Adj Close'].resample('M').ffill().pct_change()
